40d62cf6d443bb49b835d00935c83b2c44751136
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-04-25 23:51:14 -0500

    list es un arreglo que se puede iterar para renderizar componentes. Para crear expresiones dentro de los componentes, se usan las llaves {} y permiten crear expresiones de JS para generar código html usando jsx. Se itera con el iterador map y se itera la lista de películas: por una de la lista de películas que se reciban, se renderiza uno de los componentes llamado MovieCard y solo se itera una lista y dentro de una expresion, se pone el valor del nombre de la película. Es decir, se itera un listado de películas; por cada película, renderiza un componente MovieCard y dentro de la propiedad name del componente, se pasa el nombre de la película que está dentro del listado. Ahora se debe pasarle información al listado de MovieList dentro del componente principal (APP.jsx)

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,14 +3,14 @@ import { MovieCard } from "./MovieCard"
 export const MovieList = (props)=>{
 
     const {list} = props;
-    
+
     return (
         <ul>
-            <MovieCard name="Lord of the Rings" />
-            <MovieCard name="Back to the Future" />
-            <MovieCard name="Titanic" />
+            {
+                list.map((movie) => (
+                    <MovieCard name={movie.name} />
+        ))}  
         </ul>
-
     );
-}
+};
 
