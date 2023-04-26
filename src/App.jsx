1ff5a90b7ecb9cdb4408b6ab39d545312623c000
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-04-26 00:09:43 -0500

    Se le pasa información de MovieLlist dentro del componente principal. Se crea una const de las películas recientes con nombre lastestMovies que es un arreglo con un name, además, se crean algunas películas recientes. Utilizando información de lastestMovies, se pasa como una propiedad al componente MovieList. Se deben utilizar las llaves para pasar la información de JS const lastestMovies [name: 'Avatar']. Ya están las películas más recientes. Ahora se crea una lista de películas infantiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Header from "./components/Header";
 import {MovieList} from "./components/MovieList";
 
+const latestMovies = [
+  {name: "Avatar 2"},
+  {name: "Matrix 4"},
+  {name: "Thor"},
+];
+
 function App() {
   return (
     <div className="App">
     <Header />
-    <MovieList />
+    <MovieList list={latestMovies} />
     </div>
   );
 }
