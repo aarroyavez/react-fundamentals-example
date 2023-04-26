b455c2643f6e3658297dd8f204099d197f955cc5
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-04-25 23:17:30 -0500

    se crea una nueva película: Titanic usando las propiedades de los componentes para poderlos reutilizar

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,11 @@
-import { MovieCard; } from "./MovieCard"
+import { MovieCard } from "./MovieCard"
 
 export const MovieList = ()=>{
     return (
         <ul>
             <MovieCard name="Lord of the Rings" />
             <MovieCard name="Back to the Future" />
+            <MovieCard name="Titanic" />
         </ul>
 
     );
