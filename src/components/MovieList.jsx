e165a570efcd2b789e17f42280e7ed323f8a9338
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-04-25 23:14:22 -0500

    Quedan como tributos de html, las propiedades de los componentes al momento de usarlo

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,14 +3,9 @@ import { MovieCard; } from "./MovieCard"
 export const MovieList = ()=>{
     return (
         <ul>
-            <li>
-                <article>
-                    <h3>Lord of the Rings</h3>
-                    <button>Check times</button>
-                </article>
-            </li>
             <MovieCard name="Lord of the Rings" />
             <MovieCard name="Back to the Future" />
+        </ul>
 
     );
 }
